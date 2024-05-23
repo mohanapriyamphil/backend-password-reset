f96a62349f0af4e8531a189b41a6fb01c38cacfe
mohanapriyamphil <priyamphilmaths@gmail.com>
2024-05-23 19:41:24 +0530

    created port variable

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ mongoose
   .catch((err) => console.log(err.message));
 
 
-app.listen(process.env.PORT, () => {
-    console.log("listening on port " + process.env.PORT)
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log("listening on port " + port)
 })
